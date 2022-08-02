4044d0ac811d14ddbc00be6ce727c4238b76d9a6
Nildson <nildson@example.com>
2022-08-02 00:36:29 -0300

    axios e notify adicionados, services e getPokeom implementados

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,62 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="column items-center">
+      <h1>{{name}}</h1>
+      <q-img :src="url" :ratio="1" width="250px" />
+    </div>
+    <div class="row justify-around full-width">
+      <q-input filled v-model="search" label="Nome do pokemon" />
+      <q-btn color="primary" label="Pesquisar" @click="getPokemon" />
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+import api from "../services/api";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: 'IndexPage',
+
+  data () {
+    return {
+      name: "",
+      url: "",
+      search: "ivysaur",
+    }; 
+  },
+
+  async beforeMount() {
+    await this.getPokemon();
+  },
+
+  methods: {
+    getPokemon() {
+      api
+        .get(`/pokemon/${this.search}/`)
+        .then((response) => {
+          this.name = response.data.name;
+          this.url = response.data.sprites.other.dream_world.front_default;
+          this.triggerPositive();
+        })
+        .catch((err) => {
+          this.triggerNegative();
+        });
+    },
+    triggerPositive () {
+      this.$q.notify({
+        type: 'positive',
+        position: 'top',
+        message: 'Pokemon Encontrado.'
+      })
+    },
+    triggerNegative () {
+      this.$q.notify({
+        type: 'negative',
+        position: 'top',
+        message: 'Pokemon NÃO Encontrado.'
+      })
+    },
+  },
+});
 </script>
